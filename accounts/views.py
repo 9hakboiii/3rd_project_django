9d8b24b5fdcaa0692fa3c94d9d2633bcf8463874
9hakboiii <gyuhakarmy@gmail.com>
2025-04-17 17:58:26 +0900

    set: login, logout path 설정

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+"""
+class HTTPRequest:
+    POST = {
+        "username": "admin",
+        "password": "1234",
+    }
+"""
 
 
-# Create your views here.
-def login(request):
+def login_user(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "로그인 성공")
+            return redirect("/")
+        else:
+            messages.error(request, "로그인 실패")
+            return redirect("accounts:login_user")
+
     return render(request, "accounts/login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
