f10e64f64b5725e129a9f04e69586bb586c2cf62
9hakboiii <gyuhakarmy@gmail.com>
2025-04-18 20:58:34 +0900

    set: upload url 등록

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mainpage.urls")),
     path("accounts/", include("accounts.urls")),
+    path("upload/", include("upload.urls")),
 ]
 
 if settings.DEBUG:
