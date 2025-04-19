f34224d8717de98b6446b597ccf12e1ab3e532af
9hakboiii <gyuhakarmy@gmail.com>
2025-04-19 16:02:01 +0900

    model: Content 모델 생성

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Content(models.Model):
+
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    url_image = models.ImageField(upload_to="upload/url")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
