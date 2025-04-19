00dc3619482ba99352186e1517cfd0749195ffbe
9hakboiii <gyuhakarmy@gmail.com>
2025-04-19 16:02:47 +0900

    set: upload_url view 함수 연결

--- upload/urls.py
+++ upload/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from upload import views
 
@@ -6,3 +8,7 @@ app_name = "upload"
 urlpatterns = [
     path("upload/", views.upload_url, name="upload_url"),
 ]
+
+# 개발 환경에서 미디어 파일 제공
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
