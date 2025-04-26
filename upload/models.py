490aa76d074fb08eb7390ad5e5402b3cf69ba817
9hakboiii <gyuhakarmy@gmail.com>
2025-04-26 15:56:15 +0900

    model: 폰트 이름과 정보 필드 생성 및 마이그레이션

--- upload/models.py
+++ upload/models.py
@@ -6,5 +6,7 @@ class Content(models.Model):
 
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     url_image = models.ImageField(upload_to="upload/url", max_length=500)
+    font_name = models.CharField(max_length=50, blank=True, null=True, default=None)
+    font_info = models.CharField(max_length=2000, blank=True, null=True, default=None)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
